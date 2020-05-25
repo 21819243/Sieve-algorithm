2d2aa3ef6faeccc91526d8e2dcf09b093bc04283
Salih Şahin <slhshntr@gmail.com>
2020-05-25 12:00:06 +0300

    html updated and sieve algorithm added to js file.

--- sieve.js
+++ sieve.js
@@ -1,5 +1,23 @@
 function FindPrimes() {
     var selectedNumber = document.getElementById("selected_number").value;
     var primeNumbers = [];
-    var sqrtNumber = [Math.floor(Math.sqrt(t))];
+    var sqrtNumber = [Math.floor(Math.sqrt(selectedNumber))];
+    sqrtNumber[0] = 1;
+    sqrtNumber[1] = 1;
+
+    for (var i = 2; i * i <= selectedNumber; i++) {
+        if (sqrtNumber[i] !== 1) {
+            for (var z = i * i; z <= selectedNumber; z += i) {
+                sqrtNumber[z] = 1;
+            }
+        }
+    }
+
+    for (var i = 0; i <= selectedNumber; i++) {
+        if (sqrtNumber[i] !== 1) {
+            primeNumbers.push(i);
+        }
+    }
+
+    document.getElementById("resultDiv").innerHTML = primeNumbers;
 }
